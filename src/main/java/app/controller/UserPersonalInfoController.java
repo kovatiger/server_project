d0ceb1aef2344716265a3f828fbf6dfe0c3b544e
kovatiger <kovalevvvlad981@gmail.com>
2022-04-25 00:41:18 +0300

    add UserPersonalInfo

---
+++ src/main/java/app/controller/UserPersonalInfoController.java
@@ -0,0 +1,9 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:8080")
+@RestController
+public class UserPersonalInfoController {
+}
