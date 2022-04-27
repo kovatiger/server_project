e3f5fa84cbce7cbd87acb0d0190345caf8613ab2
kovatiger <kovalevvvlad981@gmail.com>
2022-04-27 19:53:26 +0300

    finish project without tests

--- src/main/java/app/repository/FlightRepository.java
+++ src/main/java/app/repository/FlightRepository.java
@@ -1,14 +1,20 @@
 package app.repository;
 
 import app.entity.Flight;
+import app.entity.Time;
+import app.entity.Way;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface FlightRepository extends JpaRepository<Flight,Long> {
     @Query("from Flight group by wayId")
     List<Flight> findAllPopularFlights();
+
+    Flight findByTimeIdAndWayId(Time timeId, Way wayId);
 }
--- src/main/java/app/repository/TripRepository.java
+++ src/main/java/app/repository/TripRepository.java
@@ -1,9 +1,21 @@
 package app.repository;
 
+import app.entity.Flight;
 import app.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface TripRepository extends JpaRepository<Trip, Long> {
+    Trip findByDateOfTripAndFlightId(String dateOfTRip, Flight flightId);
+
+    @Query("update Trip set place =:place where flightId =:flight and dateOfTrip =:date")
+    @Modifying
+    void updateAmountOfPlaces(@Param("place") Integer place,@Param("flight") Flight flight,@Param("date") String date);
 }
